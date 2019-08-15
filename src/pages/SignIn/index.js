201477cf247a66b76597ae0193c9fc1c02299484
Alisson <alissontecnico@gmail.com>
2019-08-15 17:22:38 -0300

    Validações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
@@ -9,10 +10,17 @@ export default function Signin() {
     console.tron.log(data);
   }
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Insira seu e-mail" />
         <Input name="password" type="password" placeholder="Insira sua senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
@@ -9,10 +10,20 @@ export default function Signup() {
     console.tron.log(data);
   }
 
+const schema = Yup.object().shape({
+  nome: Yup.string()
+  .required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().min(6, 'No minimo 6 caractéres').required('A senha é obrigatória'),
+});
+
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="nome" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Insira seu e-mail" />
         <Input name="password" type="password" placeholder="Insira sua senha" />
